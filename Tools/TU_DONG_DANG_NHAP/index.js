7684cc57f8b0acc62b60af0604359091b2c97476
Nguyễn Thị Hà B23DCCC057 <165117008+nguyenha2005udu@users.noreply.github.com>
2024-09-19 09:10:32 +0700

    Update index.js

--- Tools/TU_DONG_DANG_NHAP/index.js
+++ Tools/TU_DONG_DANG_NHAP/index.js
@@ -1,16 +1,12 @@
-// index.js
-
 const mysql = require('mysql2');
 
-// Tạo kết nối tới MySQL
 const connection = mysql.createConnection({
-  host: 'localhost',    // Địa chỉ của MySQL server
-  user: 'root',         // Tên người dùng MySQL
-  password: 'password', // Mật khẩu của người dùng MySQL
-  database: 'test'      // Tên cơ sở dữ liệu
+  host: 'localhost',    
+  user: 'root',         
+  password: 'password', 
+  database: 'test'      
 });
 
-// Kết nối tới MySQL
 connection.connect((err) => {
   if (err) {
     console.error('Error connecting to MySQL:', err.stack);
@@ -19,7 +15,6 @@ connection.connect((err) => {
   console.log('Connected to MySQL as id ' + connection.threadId);
 });
 
-// Thực hiện truy vấn
 connection.query('SELECT * FROM your_table', (err, results, fields) => {
   if (err) {
     console.error('Error executing query:', err.stack);
@@ -28,5 +23,4 @@ connection.query('SELECT * FROM your_table', (err, results, fields) => {
   console.log('Results:', results);
 });
 
-// Đóng kết nối
 connection.end();
